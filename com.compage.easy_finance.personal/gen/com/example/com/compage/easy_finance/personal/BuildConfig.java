8dd6d806a744fe923087d6d7a89630005ecd2515
Sterling Diaz <sterlingdiazd@gmail.com>
2014-01-23 14:06:04 -0400

    subida

---
+++ com.compage.easy_finance.personal/gen/com/example/com/compage/easy_finance/personal/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.com.compage.easy_finance.personal;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
